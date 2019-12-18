10cf692deaa278cb6583958117268193ebb59bab
Angel Valdivia <angelv1901@gmail.com>
2019-12-17 23:38:52 -0500

    clear task btn deletes task using whileloop

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Difine UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
 //Load all event listeners
 loadEventListeners();
@@ -11,18 +11,20 @@ loadEventListeners();
 // Load all event listeners
 function loadEventListeners() {
 	//Add task event
-	form.addEventListener('submit', addTask);
-	//Remove task event 
-	taskList.addEventListener('click',removeTask);
+	form.addEventListener("submit", addTask);
+	//Remove task event
+	taskList.addEventListener("click", removeTask);
+	//Clear Taskevent
+	clearBtn.addEventListener("click", clearTasks);
 }
 // Add Task
 function addTask(e) {
-	if (taskInput.value === ' ') {
+	if (taskInput.value === " ") {
 		alert("Add a task");
 	}
 
 	//Create li element
-	const li = document.createElement('li');
+	const li = document.createElement("li");
 	// add name
 	li.className = "collection-item";
 	// create text node and apend to li
@@ -39,15 +41,25 @@ function addTask(e) {
 	taskList.appendChild(li);
 
 	//Clear input
-	taskInput.value = ' ';
+	taskInput.value = " ";
 
 	e.preventDefault();
 }
 
-//Remove task 
+//Remove task
 function removeTask(e) {
-	if(e.target.parentElement.classList.contains
-		('delete-item')) {
-	e.target.parentElement.parentElement.remove();
+	if (e.target.parentElement.classList.contains("delete-item")) {
+		if (confirm("Are you Sure?")) {
+			e.target.parentElement.parentElement.remove();
+		}
+	}
+}
+// clear task
+function clearTasks() {
+	//taskList.innerHTML = '';
+
+	//Faster
+	while (taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
 	}
 }
